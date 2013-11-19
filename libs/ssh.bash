8e72ec7a6c251eff03dc1612f4a7a17f6f68b1b5
Xavier Logerais (Numericable - Ingénierie Système) <xlogerais@ncnumericable.com>
2013-11-19 17:32:05 +0100

    Add lib for ssh with function ssh_clean_known_hosts

---
+++ libs/ssh.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function ssh_clean_known_hosts ()
+{
+	if [ -z "$1" ]
+	then
+		echo "No arguments given. Cleaning ~/.ssh/known_hosts"
+		sed -i -e '/no hostip for proxy command/ d' ~/.ssh/known_hosts
+	else
+		hostname=$1
+		ip=$(ssh admin dig +short $1)
+		echo "Removing host ${hostname} from ~/.ssh/known_hosts"
+		sed -i -e "/${hostname}/ d" ~/.ssh/known_hosts
+		echo "Removing ip ${ip} from ~/.ssh/known_hosts"
+		sed -i -e "/${ip}/ d" ~/.ssh/known_hosts
+	fi
+
+}
