b62147b1778f4a53492ef73731c8327a9e636888
Xavier Logerais (Numericable - Ingénierie Système) <xlogerais@ncnumericable.com>
2013-12-05 13:41:08 +0100

    Modified function term_change_title

--- libs/term.bash
+++ libs/term.bash
@@ -5,12 +5,12 @@ function term_change_title
 	case $TERM in
 		# Change the window title of X terminals 
 		xterm*|rxvt*|urxvt*|Eterm)
-			PROMPT_COMMAND='echo -ne "\033]0;${1}\007"'
+			echo -ne "\033]0;${1}\007"
 			;;
 
 		# Change the window title of screen terminals 
 		screen*)
-			PROMPT_COMMAND='echo -ne "\033k${1}\033\\"'
+			echo -ne "\033k${1}\033\\"
 			;;
 	esac
 
