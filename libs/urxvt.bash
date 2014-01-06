77958e1d97c4b2da5551d56d5f682ef5f0d4f8d4
Xavier Logerais (Numericable - Ingénierie Système) <xlogerais@ncnumericable.com>
2014-01-06 14:49:03 +0100

    Modified lib for urxvt : add zp and zm functions to allow font zooming

--- libs/urxvt.bash
+++ libs/urxvt.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
+URXVT_FONT_NAME="DejaVu Sans Mono"
+URXVT_FONT_SIZE=10
+URXVT_FONT_INCREMENT=1
+
 function urxvt_set_font
 {
+        # set font
 	printf '\33]50;%s\007' "xft:$1"
 }
 
@@ -8,4 +13,16 @@ function urxvt_set_title
 {
           # set window title
           printf '\33]2;%s\007' "$1"
+}
+
+# Quick functions
+
+zp() {
+	URXVT_FONT_SIZE=$(echo "$URXVT_FONT_SIZE+$URXVT_FONT_INCREMENT" | bc )
+	urxvt_set_font "${URXVT_FONT_NAME}:size=${URXVT_FONT_SIZE}"
+}
+
+zm() {
+	URXVT_FONT_SIZE=$(echo "$URXVT_FONT_SIZE-$URXVT_FONT_INCREMENT" | bc )
+	urxvt_set_font "${URXVT_FONT_NAME}:size=${URXVT_FONT_SIZE}"
 }
