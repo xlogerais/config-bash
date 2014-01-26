2f11992488fcb4c0de28e82c3ff8be48af08e06a
Xavier Logerais <xavier@logerais.com>
2014-01-26 13:01:33 +0100

    Add function note

---
+++ libs/note.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+note () {
+    # if file doesn't exist, create it
+    if [[ ! -f $HOME/.notes ]]; then
+        touch $HOME/.notes
+    fi
+
+    if [[ $# -eq 0 ]]; then
+        # no arguments, print file
+        cat $HOME/.notes
+    elif [[ "$1" == "-c" ]]; then
+        # clear file
+        echo "" > $HOME/.notes
+    else
+        # add all arguments to file
+        echo "$@" >> $HOME/.notes
+    fi
+}
