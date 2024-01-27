0b9532e95bd1c5f4285082fcc5aac74c3c086d42
Xavier Logerais <xavier@logerais.com>
2024-01-27 19:24:35 +0100

    feat(libs): Amélioration de la fonction genpasswd

--- libs/password.bash
+++ libs/password.bash
@@ -14,8 +14,10 @@ genpasswd() {
        	[ "$l" == "" ] && l=12
       	local password_clear=$(tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs)
         local password_md5hash=$(openssl passwd -1 $password_clear)
-        local password_sha1=$(echo -n $password_clear | openssl dgst -sha1)
+        local password_sha1=$(echo -n "$password_clear" | openssl dgst -sha1)
+        local password_base64=$(echo -n "$password_clear" | base64)
         echo "Password : $password_clear"
+        echo "Base64 : $password_base64"
         echo "MD5HASH  : $password_md5hash"
         echo "SHA1  : $password_sha1"
 }
