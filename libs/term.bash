2aaf279c9e18cd5d3f02c33929324b6ab7fe7000
Xavier Logerais <xavier.logerais-prestataire@ca-gip.fr>
2025-03-27 11:35:46 +0000

    feat(libs): Prise en compte de tmux dans la fonction term_change_title

--- libs/term.bash
+++ libs/term.bash
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-function term_change_title
-{
+function term_change_title {
 	case $TERM in
-		# Change the window title of X terminals 
-		xterm*|rxvt*|urxvt*|Eterm)
-			echo -ne "\033]0;${1}\007"
-			;;
+	# Change the window title of X terminals
+	xterm* | rxvt* | urxvt* | Eterm)
+		echo -ne "\033]0;${1}\007"
+		;;
 
-		# Change the window title of screen terminals 
-		screen*)
-			echo -ne "\033k${1}\033\\"
-			;;
+	# Change the window title of screen terminals
+	screen* | tmux*)
+		echo -ne "\033k${1}\033\\"
+		;;
 	esac
 
 }
